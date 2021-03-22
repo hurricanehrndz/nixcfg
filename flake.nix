d7a75264c47996acc0edf908d702a71abcb16795
Carlos Hernandez <carlos@techbyte.ca>
2021-03-21 21:00:57 -0600

    Drop MacOS for now, and remove unnecessary code

--- flake.nix
+++ flake.nix
@@ -9,61 +9,38 @@
   };
 
 
-  outputs = { self, ... } @ inputs:
-    with inputs.nixpkgs.lib;
-    let
-      forEachSystem = genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
-      pkgsBySystem = forEachSystem (system:
-        import inputs.nixpkgs {
-          inherit system;
-        }
-      );
-    in
-    {
-      # `internal` isn't a known output attribute for flakes. It is used here to contain
-      # anything that isn't meant to be re-usable.
-      internal = {
-        homeConfigurations = {
-          darwin = inputs.home-manager.lib.homeManagerConfiguration {
-            configuration = { pkgs, config, ... }:
-            {
-              home.stateVersion = "20.09";
-              home.packages = with pkgs; [
-                htop
-              ];
+  outputs = { self, ... }@inputs:
+  {
+    # `internal` isn't a known output attribute for flakes. It is used here to contain
+    # anything that isn't meant to be re-usable.
+    internal = {
+      homeConfigurations = {
+        linux = inputs.home-manager.lib.homeManagerConfiguration {
+          configuration = { pkgs, config, ... }:
+          {
+            imports = [ (import ./home/modules/programs) ];
+            nixpkgs = {
+              config.allowUnfree = true;
+              overlays = [ self.overlay ];
             };
-            system = "x86_64-darwin";
-            homeDirectory = "/Users/chernand";
-            username = "chernand";
-          };
-
-
-          linux = inputs.home-manager.lib.homeManagerConfiguration {
-            configuration = { pkgs, config, ... }:
-            {
-              imports = [ (import ./home/modules/programs) ];
-              nixpkgs = {
-                config.allowUnfree = true;
-                overlays = [ self.overlay ];
-              };
-              home.stateVersion = "20.09";
-              home.packages = with pkgs; [
-                htop
-              ];
-              programs.sheldon = {
-                enable = true;
-                settings = {
-                  shell = "zsh";
-                };
+            home.stateVersion = "20.09";
+            home.packages = with pkgs; [
+              htop
+            ];
+            programs.sheldon = {
+              enable = true;
+              settings = {
+                shell = "zsh";
               };
             };
-            system = "x86_64-linux";
-            homeDirectory = "/home/users/hurricanehrndz";
-            username = "hurricanehrndz";
           };
+          system = "x86_64-linux";
+          homeDirectory = "/home/users/hurricanehrndz";
+          username = "hurricanehrndz";
         };
       };
-
-      overlay = final: prev: (import ./nix/pkgs/packages.nix) final prev;
     };
+
+    overlay = final: prev: (import ./nix/pkgs/packages.nix) final prev;
+  };
 }
