45f905e4c0011328f21852b9b10e3e04e002fef8
Carlos Hernandez <carlos@techbyte.ca>
2021-03-26 22:10:19 -0600

    Add some sensible neovim settings
    Disable python plugin providers, attempt to go native or use lua.

--- home/configs/neovim/nvim/init.lua
+++ home/configs/neovim/nvim/init.lua
@@ -1,15 +1,24 @@
 local api = vim.api                   -- vim api
 local execute = api.nvim_command      -- to execute Vim commands e.g. cmd('pwd')
 local fn = vim.fn                     -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g                       -- a table to access global variables
+local g = vim.g                       -- global settings
 
+-- Disable python plugin support
+g.loaded_python_provider = 0
+g.loaded_python3_provider = 0
+
+-- Sensible defaults
+require('settings')
 
 local packer_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 local packer_repo = 'https://github.com/wbthomason/packer.nvim'
 local packer_install_cmd =
     '!git clone ' .. ' ' .. packer_repo .. ' ' .. packer_path
 
--- install packer if missing
+-- Install packer if missing
 if fn.empty(fn.glob(packer_path)) > 0 then
   execute(packer_install_cmd)
 end
+
+-- Install plugins
+require('plugins')
