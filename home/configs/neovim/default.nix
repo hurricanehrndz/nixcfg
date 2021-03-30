018fe614f90f18dcdb4613cc8240e125217184dd
Carlos Hernandez <carlos@techbyte.ca>
2021-03-29 19:22:47 -0600

    Neovim: attempt to fix tree-sitter

--- home/configs/neovim/default.nix
+++ home/configs/neovim/default.nix
@@ -10,14 +10,29 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       neovim-nightly
-      rnix-lsp
       nodePackages.npm
-      nodePackages.bash-language-server
-      nodePackages.pyright
+
+      nodePackages.typescript
       nodePackages.typescript-language-server
+
+      nodePackages.bash-language-server
       nodePackages.vim-language-server
       nodePackages.yaml-language-server
+      nodePackages.dockerfile-language-server-nodejs
+      nodePackages.dockerfile-language-server-nodejs
+
+      nodePackages.pyright
+
+      # Language Server
       sumneko-lua-language-server
+      rnix-lsp
+      terraform-ls
+
+      # Formatters
+      nodePackages.prettier
+      nixfmt
+      rustfmt
+      terraform
     ];
 
     xdg.configFile = {
@@ -25,7 +40,12 @@ in
         recursive = true;
         source = ./nvim;
       };
-      "nvim/parser/c.so".source = "${pkgs.tree-sitter.builtGrammars.c}/parser";
     };
+    # Add all tree-sitter parsers
+    home.file = lib.attrsets.mapAttrs'
+      (name: drv: lib.attrsets.nameValuePair
+        ("${config.xdg.configHome}/nvim/parser/" + (lib.strings.removePrefix "tree-sitter-" name) + ".so")
+        { source = "${drv}/parser"; })
+      pkgs.tree-sitter.builtGrammars;
   };
 }
