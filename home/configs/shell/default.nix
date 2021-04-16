60695e36713ab51cb7c92ded6bc0dddda4a164de
Carlos Hernandez <carlos@techbyte.ca>
2021-04-16 06:38:50 -0600

    Enable zsh-vi-mode - fix formatting

--- home/configs/shell/default.nix
+++ home/configs/shell/default.nix
@@ -1,10 +1,8 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-let
-  cfg = config.hurricane.configs.shell;
-in
-{
+let cfg = config.hurricane.configs.shell;
+in {
   options.hurricane = {
     configs.shell.enable = mkEnableOption "enable awsome zsh config";
   };
@@ -21,14 +19,14 @@ in
           };
         }
         {
-          name = "zsh-utils";
-          file = "directory.plugin.zsh";
-          src = ./zsh-utils;
-        }
-        {
-          name = "zsh-utils";
-          file = "editor.plugin.zsh";
-          src = ./zsh-utils;
+          # https://github.com/starship/starship/issues/1721#issuecomment-780250578
+          # stop eating lines this is not pacman
+          name = "zsh-vi-mode";
+          file = "zsh-vi-mode.plugin.zsh";
+          src = builtins.fetchGit {
+            url = "https://github.com/jeffreytse/zsh-vi-mode";
+            rev = "2cdeb68d5eab63a8bd951aec52bb407b8445fb1a";
+          };
         }
         {
           name = "zsh-utils";
@@ -37,7 +35,7 @@ in
         }
         {
           name = "zsh-utils";
-          file = "completion.plugin.zsh";
+          file = "directory.plugin.zsh";
           src = ./zsh-utils;
         }
         {
@@ -49,7 +47,8 @@ in
         {
           name = "zsh-history-substring-search";
           file = "zsh-history-substring-search.zsh";
-          src = "${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search";
+          src =
+            "${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search";
         }
         {
           name = "zsh-autosuggestions";
@@ -57,36 +56,73 @@ in
           src = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
           apply = "zsh-defer";
         }
+        {
+          name = "zsh-utils";
+          file = "completion.plugin.zsh";
+          src = ./zsh-utils;
+        }
       ];
     };
 
     # Enable shell management
     programs.zsh = {
       enable = true;
-     # autocd = true;
+      # autocd = true;
       enableCompletion = false; # Enable when ready
-      #defaultKeymap = "viins";
       dotDir = ".config/zsh";
       history = {
         extended = true;
         expireDuplicatesFirst = true;
         ignoreDups = true;
         ignoreSpace = true;
-        path =  "${config.xdg.configHome}/zsh/zsh_history";
+        path = "${config.xdg.configHome}/zsh/zsh_history";
       };
-
-      initExtra = ''
+      initExtraBeforeCompInit = ''
+        # XDG bin
         path=("$HOME/.local/bin" $path)
+
         # Nix setup (environment variables, etc.)
         if [[ -e ~/.nix-profile/etc/profile.d/nix.sh ]] \
             && [[ -z "$NIX_SSL_CERT_FILE" ]]; then
           source ~/.nix-profile/etc/profile.d/nix.sh
         fi
+
         # Load environment variables from a file; this approach allows me to not
         # commit secrets like API keys to Git
         if [ -e ~/.env ]; then
           source ~/.env
         fi
+
+        # Inserts 'sudo ' at the beginning of the line.
+        function prepend_sudo {
+          if [[ "$BUFFER" != su(do|)\ * ]]; then
+            BUFFER="sudo $BUFFER"
+            (( CURSOR += 5 ))
+          fi
+        }
+        zle -N prepend_sudo
+
+        autoload -U edit-command-line
+        zle -N edit-command-line
+
+        # Define an init function and append to zvm_after_init_commands
+        function zvm_after_init() {
+          source ${pkgs.skim}/share/skim/completion.zsh
+          source ${pkgs.skim}/share/skim/key-bindings.zsh
+
+          # my key bindings
+          bindkey   -M   viins   '\C-X\C-S'      prepend_sudo
+          bindkey   -M   viins   '\C-Y'          autosuggest-accept
+          bindkey   -M   viins   '\C-K'          history-substring-search-up
+          bindkey   -M   viins   '\C-J'          history-substring-search-down
+          bindkey   -M   vicmd   'k'             history-substring-search-up
+          bindkey   -M   vicmd   'j'             history-substring-search-down
+          bindkey   -M   vicmd   '\C-X\C-E'      edit-command-line
+          bindkey   -M   viins   '\C-X\C-E'      edit-command-line
+        }
+      '';
+      initExtra = ''
+        ZVM_CURSOR_STYLE_ENABLED=false
       '';
       shellAliases = with pkgs; {
         # Aliases that make commands colourful.
@@ -98,14 +134,11 @@ in
         "ll" = "${exa}/bin/exa -lhF --group-directories-first";
         "la" = "${exa}/bin/exa -alhF --group-directories-first";
         "lt" = "${exa}/bin/exa -alhF --sort modified";
-        "l"  = "${exa}/bin/exa -1 --group-directories-first -F";
+        "l" = "${exa}/bin/exa -1 --group-directories-first -F";
         "tree" = "${exa}/bin/exa -T";
         # Aliases for `cat` to `bat`.
         "cat" = "${bat}/bin/bat";
       };
-    };
-    programs.skim = {
-      enable = true;
     };
     programs.starship = {
       enable = true;
@@ -116,6 +149,8 @@ in
           fish_style_pwd_dir_length = 1;
         };
         cmd_duration.disabled = true;
+        aws.disabled = true;
+        scan_timeout = 10;
       };
     };
     # Enable XDG User Directories
@@ -126,6 +161,8 @@ in
       (writeScriptBin "print-colors" (builtins.readFile ./print-colors))
       (writeScriptBin "test-truecolors" (builtins.readFile ./test-truecolors))
       (writeScriptBin "rtfm" (builtins.readFile ./rtfm))
+      # fuzzy finder
+      skim
       # curl
       curl
       # gnu awk
