8599740a297851d3508fc7ace0c17db57580a438
Carlos Hernandez <carlos@techbyte.ca>
2021-04-18 16:20:15 -0600

    Refactor and document neovim config

--- home/configs/neovim/default.nix
+++ home/configs/neovim/default.nix
@@ -29,26 +29,32 @@ in
       vimAlias = true;
       vimdiffAlias = true;
       extraConfig = ''
-        lua require'init'
+        lua require('init')
       '';
 
       extraPackages = with pkgs; [
         nodePackages.neovim
-        nodePackages.typescript
-        nodePackages.typescript-language-server
+        # typescript is needed because it provides the tsserver command.
+        # First, it will try to find a tsserver installed with npm install,
+        # if not found, it will look in our $PATH
+        # See https://github.com/theia-ide/typescript-language-server/blob/a92027377b7ba8b1c9318baad98045e5128baa8e/server/src/lsp-server.ts#L75-L94
+        # See https://github.com/jlesquembre/dotfiles/blob/23d7906e5de3dbf4e9433dffeb23c95d3d9a9d06/home-manager/neovim.nix#L169
 
-        nodePackages.bash-language-server
-        nodePackages.vim-language-server
-        nodePackages.yaml-language-server
-        nodePackages.dockerfile-language-server-nodejs
-        nodePackages.dockerfile-language-server-nodejs
-
-        nodePackages.pyright
-
-        # Language Server
+        # Language Servers
+        rls
         rnix-lsp
         terraform-ls
 
+        nodePackages.bash-language-server
+        nodePackages.dockerfile-language-server-nodejs
+        nodePackages.pyright
+        nodePackages.typescript
+        nodePackages.typescript-language-server
+        nodePackages.vim-language-server
+        nodePackages.yaml-language-server
+
+        # Tree-Sitter
+        # see: https://github.com/breuerfelix/nixos/commit/f6aaf6d75e9a847a91e16314752c9d6614e1b04c#diff-fad35a4bb4e2335be1b89e3563ec9be3946208467db9de26226674db7469b6d4R41-R44
         gcc gccStdenv
 
         # Formatters
