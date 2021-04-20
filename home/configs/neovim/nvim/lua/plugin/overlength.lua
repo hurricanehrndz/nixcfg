882cb74b065488c67f09f7ccc6da0f6e598510bc
Carlos Hernandez <carlos@techbyte.ca>
2021-04-19 19:32:23 -0600

    Fix check for overlength loading

--- home/configs/neovim/nvim/lua/plugin/overlength.lua
+++ home/configs/neovim/nvim/lua/plugin/overlength.lua
@@ -1,10 +1,12 @@
-if (vim.g.loaded_overlength == nil) then
+local loaded_overlength, _ = pcall(function() return vim.g.loaded_overlength end)
+if (not loaded_overlength) then
  do return end
 end
 
 vim.g['overlength#default_overlength'] = 120
 vim.g['overlength#default_to_textwidth'] = 1
 vim.g['overlength#default_grace_length'] = 1
-vim.fn['overlength#disable_filetypes']({ 'markdow', 'vimwiki', 'startify', 'term', 'man', 'qf', '' })
+vim.fn['overlength#disable_filetypes']({ 'markdown', 'vimwiki', 'startify', 'term', 'man', 'qf', '' })
 vim.fn['overlength#set_overlength']('text', 80)
 vim.fn['overlength#set_overlength']('startify', 0)
+vim.fn['overlength#set_overlength']('markdown', 0)
