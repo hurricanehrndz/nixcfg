089b89b226dec104dc751e07824415661ec06a0d
Carlos Hernandez <carlos@techbyte.ca>
2021-04-21 20:49:30 -0600

    Disable overlength on commit messages

--- home/configs/neovim/nvim/lua/plugin/overlength.lua
+++ home/configs/neovim/nvim/lua/plugin/overlength.lua
@@ -6,7 +6,7 @@ end
 vim.g['overlength#default_overlength'] = 120
 vim.g['overlength#default_to_textwidth'] = 1
 vim.g['overlength#default_grace_length'] = 1
-vim.fn['overlength#disable_filetypes']({ 'markdown', 'vimwiki', 'startify', 'term', 'man', 'qf', '' })
+vim.fn['overlength#disable_filetypes']({ 'gitcommit', 'markdown', 'vimwiki', 'startify', 'term', 'man', 'qf', '' })
 vim.fn['overlength#set_overlength']('text', 80)
 vim.fn['overlength#set_overlength']('startify', 0)
 vim.fn['overlength#set_overlength']('markdown', 0)
