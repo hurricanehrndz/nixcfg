6a7a61808896234abc5c14e7a7d7e22df997e779
Carlos Hernandez <carlos@techbyte.ca>
2021-04-23 14:58:42 -0600

    Update lsp config
    Tweak maps to use vim.keymap. Enable buffer aware keymaps for saga.
    Disable virtual text for diagnostics.

--- home/configs/neovim/nvim/lua/hrndz/lsp/init.lua
+++ home/configs/neovim/nvim/lua/hrndz/lsp/init.lua
@@ -1,72 +1,76 @@
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+local custom_attach = function(client)
+  vim.bo.omnifunc = 'v:lua.vim.lsp.omnifunc'
+  local nnoremap = vim.keymap.nnoremap
+  local vnoremap = vim.keymap.vnoremap
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  local has_saga, _ = pcall(require, "lspsaga")
+  if has_saga then
+    nnoremap({"[d", function() require("lspsaga.diagnostic").lsp_jump_diagnostic_prev() end, buffer = 0})
+    nnoremap({"]d", function() require("lspsaga.diagnostic").lsp_jump_diagnostic_next() end, buffer = 0})
+  end
 
-  -- Mappings.
-  local opts = {noremap = true, silent = true}
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  nnoremap({"gD", vim.lsp.buf.declaration, buffer = 0})
+  nnoremap({"gd", vim.lsp.buf.definition, buffer = 0})
+  nnoremap({"gi", vim.lsp.buf.implementation, buffer = 0})
+  nnoremap({"<space>wa", vim.lsp.buf.add_workspace_folder, buffer = 0})
+  nnoremap({"<space>wr", vim.lsp.buf.remove_workspace_folder, buffer = 0})
+  nnoremap({'<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, buffer = 0})
+  nnoremap({"<space>D", vim.lsp.buf.type_definition, buffer = 0})
+  nnoremap({"gr", vim.lsp.buf.references, buffer = 0})
+  nnoremap({"<space>q", vim.lsp.diagnostic.set_loclist, buffer = 0})
 
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    nnoremap({",f", vim.lsp.buf.formatting, buffer = 0})
   end
   if client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-  end
-
-  -- Set autocommands conditional on server_capabilities
-  -- Enable reference highlight
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
+    vnoremap({",f", vim.lsp.buf.range_formatting, buffer = 0})
   end
 end
 
-local nvim_lsp = require('lspconfig')
+local nvim_lsp = require("lspconfig")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+local lsp = vim.lsp
+
+-- Disable diagnostic virtual text
+lsp.handlers["textDocument/publishDiagnostics"] = lsp.with(
+  lsp.diagnostic.on_publish_diagnostics, {
+    -- Disable virtual_text
+    virtual_text = false,
+  }
+)
 
 -- Enable snippet support from hrsh7th/vim-vsnip
 capabilities.textDocument.completion.completionItem.snippetSupport = true;
-capabilities.textDocument.completion.completionItem.resolveSupport =
-  {properties = {'documentation', 'detail', 'additionalTextEdits'}}
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits",
+  }
+}
 
 local servers = {"pyright", "bashls", "dockerls", "vimls", "tsserver", "rust_analyzer"}
-for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {capabilities = capabilities, on_attach = on_attach} end
+for _, lsp_server in ipairs(servers) do
+  nvim_lsp[lsp_server].setup({
+    on_attach = custom_attach,
+    capabilities = capabilities,
+  })
+end
 
 -- lua
 local lua_lsp_location = vim.fn.expand("~/.local/share/lua-lsp")
 local lua_lsp = string.format("%s/lua-language-server", lua_lsp_location)
 local lua_lsp_build_file = string.format("%s/main.lua", lua_lsp_location)
-require('nlua.lsp.nvim').setup(require('lspconfig'), {
-  on_attach = on_attach,
+require("nlua.lsp.nvim").setup(require("lspconfig"), {
+  on_attach = custom_attach,
   capabilities = capabilities,
-  cmd = {lua_lsp, "-E", lua_lsp_build_file}
+  cmd = {lua_lsp, "-E", lua_lsp_build_file},
 })
 
 -- powershell
 local pwsh_bundle_location = vim.fn.expand("~/.local/share/pses")
-nvim_lsp.powershell_es.setup({bundle_path = pwsh_bundle_location})
+nvim_lsp.powershell_es.setup({
+  on_attach = custom_attach,
+  bundle_path = pwsh_bundle_location,
+})
