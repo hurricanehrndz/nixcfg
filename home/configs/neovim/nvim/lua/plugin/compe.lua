5e0769b8e949a5955fb915a61cb72af51fe7c71a
Carlos Hernandez <carlos@techbyte.ca>
2021-04-28 15:45:51 -0600

    Re-factor + enable come#confirm via C-y

--- home/configs/neovim/nvim/lua/plugin/compe.lua
+++ home/configs/neovim/nvim/lua/plugin/compe.lua
@@ -3,31 +3,35 @@ vim.o.completeopt = "menuone,noinsert,noselect"
 -- disable insert completion menu messages
 vim.o.shortmess = vim.o.shortmess .. "c"
 
-local has_compe, compe = pcall(require, 'compe')
-if has_compe then
-  compe.setup {
-    enabled = true,
-    autocomplete = true,
-    debug = false,
-    min_length = 2,
-    preselect = 'enable',
-    throttle_time = 80,
-    source_timeout = 200,
-    incomplete_delay = 400,
-    max_abbr_width = 100,
-    max_kind_width = 100,
-    max_menu_width = 100,
-    documentation = true,
-
-    source = {
-      path = true,
-      buffer = true,
-      calc = true,
-      nvim_lsp = true,
-      nvim_lua = true,
-      vsnip = true,
-      spell = true,
-      treesitter = false
-    }
-  }
+local has_compe, compe = pcall(require, "compe")
+if not has_compe then
+ do return end
 end
+
+compe.setup {
+  enabled = true,
+  autocomplete = true,
+  debug = false,
+  min_length = 2,
+  preselect = "enable",
+  throttle_time = 80,
+  source_timeout = 200,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+  documentation = true,
+
+  source = {
+    path = true,
+    buffer = true,
+    calc = true,
+    nvim_lsp = true,
+    nvim_lua = true,
+    vsnip = true,
+    spell = true,
+    treesitter = false
+  }
+}
+
+vim.cmd([[inoremap <silent><expr> <C-y>      compe#confirm("<C-y>")]])
