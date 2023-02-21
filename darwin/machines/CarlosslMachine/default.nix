d0ed8f7cf4208ce67bd0140b8aaec2262675a125
Carlos Hernandez <chernand@yelp.com>
2023-02-20 20:11:57 -0700

    Set and manager user shell via nix

--- darwin/machines/CarlosslMachine/default.nix
+++ darwin/machines/CarlosslMachine/default.nix
@@ -14,9 +14,18 @@ in {
     '';
   };
 
-  environment.systemPackages = [
-    pkgs.vim
+  environment.systemPackages = with pkgs; [
+    vim
+    ncurses
   ];
+
+  environment.shells = [pkgs.zsh];
+
+  users.users.${username} = {
+    home = "/Users/${username}";
+    isHidden = false;
+    shell = pkgs.zsh;
+  };
 
   home-manager.users.${username} = hmArgs: {
     imports = with hmArgs.roles; base;
