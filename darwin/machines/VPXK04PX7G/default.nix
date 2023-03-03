75b3525a19a9268ef422c2e65d19045c45a31f00
Carlos Hernandez <carlos@hrndz.ca>
2023-03-03 16:13:38 -0700

    Disable spotlight hotkeys

--- darwin/machines/VPXK04PX7G/default.nix
+++ darwin/machines/VPXK04PX7G/default.nix
@@ -19,15 +19,28 @@ in {
   targets.darwin.defaultsdicts = {
     # Disable Spotlight hotkeys
     "com.apple.symbolichotkeys" = {
-      "64" = {
-        enabled = 0;
-        value = {
-          type = "standard";
-          parameters = [
-            65535 # no ascii code
-            49 # 0x31 space key
-            1048576 # command key
-          ];
+      "AppleSymbolicHotKeys" = {
+        "64" = {
+          enabled = 0;
+          value = {
+            type = "standard";
+            parameters = [
+              65535 # no ascii code
+              49 # 0x31 space key
+              1048576 # command key
+            ];
+          };
+        };
+        "65" = {
+          enabled = 0;
+          value = {
+            type = "standard";
+            parameters = [
+              65535 # no ascii code
+              49 # 0x31 space key
+              1572864 # command+option
+            ];
+          };
         };
       };
     };
--- darwin/modules/defaultsdicts.nix
+++ darwin/modules/defaultsdicts.nix
@@ -1,32 +1,36 @@
-{ config, lib, inputs, pkgs, ... }:
-
-with import (inputs.darwin + "/modules/launchd/lib.nix")  { inherit lib; };
-with lib;
-
-let
+{
+  config,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
+with import (inputs.darwin + "/modules/launchd/lib.nix") {inherit lib;};
+with lib; let
   enabled = pkgs.hostPlatform.isDarwin;
   cfg = config.targets.darwin.defaultsdicts;
-  mkActivationCmds = settings:
-    let
-      toPretty = attrs: builtins.replaceStrings ["\n"] [""] (pprExpr "" attrs);
+  mkActivationCmds = settings: let
+    toPretty = attrs: builtins.replaceStrings ["\n"] [""] (pprExpr "" attrs);
 
-      toDefaultsWriteDict = domain: key: attrs: ''
-        /usr/bin/defaults write ${escapeShellArg domain} AppleSymbolicHotKeys -dict-add ${key} "${toPretty attrs}"
-      '';
+    toDefaultsWriteDictCmd = domain: dict: mapAttrsToList (forEachDictInEachDomain domain) dict;
 
-      toActivationCmd = domain: attrs: mapAttrsToList (toDefaultsWriteDict domain) attrs;
+    forEachDictInEachDomain = domain: dict: key: mapAttrsToList (forEachKeyInEachDictInDomain domain dict) key;
 
-      nonNullDefaults =
-        mapAttrs (domain: attrs: (filterAttrs (n: v: v != null) attrs))
-        settings;
+    forEachKeyInEachDictInDomain = domain: dict: key: value: ''
+      /usr/bin/defaults write ${escapeShellArg domain} ${dict} -dict-add ${key} "${toPretty value}"
+    '';
 
-      writableDefaults =
-        filterAttrs (domain: attrs: attrs != { }) nonNullDefaults;
-    in flatten (mapAttrsToList toActivationCmd writableDefaults);
+    nonNullDefaults =
+      mapAttrs (domain: attrs: (filterAttrs (n: v: v != null) attrs))
+      settings;
+
+    writableDefaults =
+      filterAttrs (domain: attrs: attrs != {}) nonNullDefaults;
+  in
+    flatten (mapAttrsToList toDefaultsWriteDictCmd writableDefaults);
 
   activationCmds = mkActivationCmds cfg;
-in
-{
+in {
   options.targets.darwin.defaultsdicts = mkOption {
     description = "Add dictionary to defaults database for a domain";
     type = types.attrsOf types.attrs;
@@ -36,7 +40,7 @@ in
   config = mkIf (enabled && cfg != {}) {
     system.activationScripts.postUserActivation.text = ''
       ${concatStringsSep "\n" activationCmds}
-      # $DRY_RUN_CMD /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+      $DRY_RUN_CMD /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
   };
 }
