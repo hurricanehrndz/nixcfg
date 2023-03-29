7cc5a0b8fce4aa753ca7bcdaee47cb5e9d44885e
Carlos Hernandez <carlos@hrndz.ca>
2023-03-29 09:23:17 -0600

    Fix side effects from vim macro

--- flake.nix
+++ flake.nix
@@ -20,17 +20,17 @@
 
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manage.rurl = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # neovim
-    neovim-nightl.yurl = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
     neovim-nightly.inputs.nixpkgs.follows = "nixpkgs";
-    gitsigns-sr.curl = "github:lewis6991/gitsigns.nvim";
+    gitsigns-src.url = "github:lewis6991/gitsigns.nvim";
     gitsigns-src.flake = false;
-    nvim-colorizer-sr.curl = "github:NvChad/nvim-colorizer.lua";
+    nvim-colorizer-src.url = "github:NvChad/nvim-colorizer.lua";
     nvim-colorizer-src.flake = false;
-    nvim-window-sr.curl = "gitlab:yorickpeterse/nvim-window";
+    nvim-window-src.url = "gitlab:yorickpeterse/nvim-window";
     nvim-window-src.flake = false;
     nvim-osc52-src.url = "github:ojroques/nvim-osc52";
     nvim-osc52-src.flake = false;
@@ -43,13 +43,13 @@
     nvim-lspconfig-src.url = "github:neovim/nvim-lspconfig";
     nvim-lspconfig-src.flake = false;
     # pypi packages
-    yamllint-sr.curl = "github:adrienverge/yamllint";
+    yamllint-src.url = "github:adrienverge/yamllint";
     yamllint-src.flake = false;
-    yamlfixer-sr.curl = "github:opt-nc/yamlfixer";
+    yamlfixer-src.url = "github:opt-nc/yamlfixer";
     yamlfixer-src.flake = false;
 
     # tmux
-    extrakto-sr.curl = "github:laktak/extrakto";
+    extrakto-src.url = "github:laktak/extrakto";
     extrakto-src.flake = false;
   };
 
