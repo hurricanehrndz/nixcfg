bee66d4dd76a88167e051fd85048b5f45ead1c4a
Carlos Hernandez <carlos@hrndz.ca>
2023-03-30 06:58:07 -0600

    Test not closing dapui automatically

--- home/profiles/nvim/config/lua/hrndz/plugins/dap.lua
+++ home/profiles/nvim/config/lua/hrndz/plugins/dap.lua
@@ -37,12 +37,12 @@ require("dapui").setup()
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open({})
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close({})
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close({})
-end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--   dapui.close({})
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--   dapui.close({})
+-- end
 
 -- enable virtual text
 local has_dap_virtual_txt, dap_virtual_txt = pcall(require, "nvim-dap-virtual-text")
