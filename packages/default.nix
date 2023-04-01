10253ec63884161bd2c57ab1c357be05eccd23b8
Carlos Hernandez <carlos@hrndz.ca>
2023-04-01 00:31:41 -0600

    Drop unnecessary boilerplate

--- packages/default.nix
+++ packages/default.nix
@@ -5,9 +5,7 @@
     ...
   }: {
     _module.args.packages = ctx.config.packages;
-    # boilerplate - TODO: use
     packages.swiftformat = pkgs.callPackage ./tools/swiftformat.nix {inherit (inputs) swiftformat-src;};
     packages.swiftlint = pkgs.callPackage ./tools/swiftlint {inherit (inputs) swiftlint-src;};
-    packages.hello = pkgs.hello;
   };
 }
