48962038e8b2906e57278e88acfd1ad23a231171
Carlos Hernandez <carlos@hrndz.ca>
2023-04-04 08:11:51 -0600

    Add nix-shell for go

---
+++ shells/go.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  inputs,
+}: let
+  inherit (inputs.devenv.lib) mkShell;
+in
+  mkShell {
+    inherit inputs pkgs;
+    modules = [
+      ({pkgs, ...}: {
+        languages.go.enable = true;
+      })
+    ];
+  }
