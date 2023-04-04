94fd2bf999ba0d623482e3ac6ddafd05b892de1d
Carlos Hernandez <carlos@hrndz.ca>
2023-04-04 08:12:18 -0600

    Add commented devenv for swift

--- shells/swift.nix
+++ shells/swift.nix
@@ -7,3 +7,28 @@ in
   mkShell {
     nativeBuildInputs = with pkgs; [swift swiftpm];
   }
+
+#     inherit (inputs.devenv.lib) mkShell;
+# in
+#   mkShell {
+#     inherit inputs pkgs;
+#     modules = [
+#       ({
+#         config,
+#         pkgs,
+#         ...
+#       }:
+#         {
+#           packages = with pkgs; [swift swiftpm];
+#           languages.swift.enable = true;
+#         }
+#         // pkgs.lib.optionals pkgs.stdenv.isDarwin {
+#           packages = with pkgs.darwin.apple_sdk; [
+#             frameworks.Foundation
+#             frameworks.CoreFoundation
+#             pkgs.swiftPackages.Foundation
+#           ];
+#           env.CFLAGS = [" -iframework ${config.env.DEVENV_PROFILE}/Library/Frameworks"];
+#         })
+#     ];
+#   }
