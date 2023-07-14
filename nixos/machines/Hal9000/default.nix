27b1744cc98d967b26ed381ad2019ee94e4fde89
Carlos Hernandez <carlos@hrndz.ca>
2023-07-14 16:26:50 -0600

    Add config for home-assistant

--- nixos/machines/Hal9000/default.nix
+++ nixos/machines/Hal9000/default.nix
@@ -16,6 +16,41 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.domain = "hrndz.ca";
 
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.homeassistant = {
+      volumes = ["/var/lib/home-assistant:/config"];
+      environment.TZ = "America/Edmonton";
+      image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
+      extraOptions = [
+        "--network=host"
+        "--pull=always"
+      ];
+    };
+    containers.zwavejs = {
+      volumes = ["/var/lib/zwavejs:/usr/src/app/store"];
+      ports = [
+        "8091:8091"
+        "3000:3000"
+      ];
+      environment.TZ = "America/Edmonton";
+      image = "zwavejs/zwave-js-ui:latest";
+      extraOptions = [
+        "--pull=always"
+        "--device=/dev/serial/by-id/usb-Silicon_Labs_Zooz_ZST10_700_Z-Wave_Stick_9a08dfb2ac21ec11bb06be942c86906c-if00-port0:/dev/zwave:rw" # Example, change this to match your own hardware
+      ];
+    };
+  };
+
+  services.logind.extraConfig = ''
+    # don’t shutdown when power button is short-pressed
+    HandlePowerKey=ignore
+    # don’t shutdown when lid is closed
+    HandleLidSwitch=ignore
+  '';
+
+  networking.firewall.allowedTCPPorts = [8123 8091];
+
   system.stateVersion = "23.05";
 }
 # vim: set et sw=2 :
