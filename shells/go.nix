a5baef0dc98720a163e0afb250f045924ca3b8d9
Carlos Hernandez <carlos@hrndz.ca>
2023-07-13 18:57:04 -0600

    Switching golang shell to devenv

--- shells/go.nix
+++ shells/go.nix
@@ -1,9 +1,43 @@
 {
   pkgs,
   inputs,
+  inputs',
   ...
-}:
-with pkgs;
+}: let
+  inherit (inputs.devenv.lib) mkShell;
+  inherit (inputs'.pdenv) packages;
+in
   mkShell {
-    buildInputs = [go];
+    inherit inputs pkgs;
+    modules = [
+      (
+        {
+          config,
+          pkgs,
+          ...
+        }: {
+          packages = with pkgs; [
+            revive
+            gopls
+            gofumpt
+            golangci-lint
+            gomodifytags
+            packages.goimports-reviser
+          ];
+          languages.go.enable = true;
+        }
+        # // pkgs.lib.optionals pkgs.stdenv.isDarwin {
+        #   packages = with pkgs.darwin.apple_sdk; [
+        #     frameworks.Foundation
+        #     frameworks.CoreFoundation
+        #     pkgs.swiftPackages.Foundation
+        #   ];
+        #   env.CFLAGS = [" -iframework ${config.env.DEVENV_PROFILE}/Library/Frameworks"];
+        # })
+      )
+    ];
   }
+# with pkgs;
+#   mkShell {
+#     buildInputs = [go gopls golangci-lint gofumpt];
+#   }
