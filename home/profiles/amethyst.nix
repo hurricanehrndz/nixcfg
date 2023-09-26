07ea251e118af5d6882574886adea61d6f21e73c
Carlos Hernandez <carlos@hrndz.ca>
2023-09-26 09:35:04 -0600

    Update amethyst config

--- home/profiles/amethyst.nix
+++ home/profiles/amethyst.nix
@@ -9,27 +9,67 @@ lib.mkMerge [
     services.amethyst.settings = {
       layouts = [
         "tall"
+        "middle-wide"
         "fullscreen"
       ];
-      select-wide-layout = "";
-      select-column-layout = "";
-      select-fullscreen-layout = {
-        mod = "mod1";
-        key = "f";
-      };
       select-tall-layout = {
         mod = "mod1";
         key = "a";
       };
-      focus-screen-1 = "";
-      focus-screen-2 = "";
-      focus-screen-3 = "";
-      focus-screen-4 = "";
-      throw-screen-1 = "";
-      throw-screen-2 = "";
-      throw-screen-3 = "";
-      throw-screen-4 = "";
-      window-max-count = 3;
+      toggle-float = {
+        mod = "mod1";
+        key = "r";
+      };
+      select-middle-wide-layout = {
+        mod = "mod1";
+        key = "s";
+      };
+      select-fullscreen-layout = {
+        mod = "mod1";
+        key = "t";
+      };
+      # disabled keys
+      select-wide-layout = {
+        mod = "mod4";
+        key = "s";
+      };
+      select-column-layout = {
+        mod = "mod4";
+        key = "t";
+      };
+      focus-screen-1 = {
+        mod = "mod4";
+        key = "w";
+      };
+      focus-screen-2 =  {
+        mod = "mod4";
+        key = "f";
+      };
+      focus-screen-3 = {
+        mod = "mod4";
+        key = "p";
+      };
+      focus-screen-4 = {
+        mod = "mod4";
+        key = "q";
+      };
+      throw-screen-1 = {
+        mod = "mod3";
+        key = "w";
+      };
+      throw-screen-2 = {
+        mod = "mod4";
+        key = "f";
+      };
+      throw-screen-3 = {
+        mod = "mod4";
+        key = "p";
+      };
+      throw-screen-4 = {
+        mod = "mod4";
+        key = "q";
+      };
+      window-max-count = 5;
       floating = [
         "com.apple.systempreferences"
         # "com.tinyspeck.slackmacgap"
