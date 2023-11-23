cf3429219242bf8152ec95711572cc3de273b9f6
Carlos Hernandez <carlos@hrndz.ca>
2023-11-22 17:51:05 -0700

    Remove unnecessary packages

--- packages/default.nix
+++ packages/default.nix
@@ -6,7 +6,6 @@
     ...
   }: {
     _module.args.packages = ctx.config.packages;
-    packages.aws-sso = pkgs.callPackage ./aws-sso {};
     packages.sonarrv4 = pkgs.callPackage ./sonarrv4 {};
     packages.pdenv = inputs'.pdenv.packages.pdenv;
   };
