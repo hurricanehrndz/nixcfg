0d55e085b49496e8a9d05222108c4182da7183ef
Carlos Hernandez <carlos@hrndz.ca>
2023-12-10 13:48:14 -0700

    Do not wrap sudo cmds in heredoc

--- packages/tools/nixos-install-init/script.sh
+++ packages/tools/nixos-install-init/script.sh
@@ -14,9 +14,6 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
-echo -e "${Red}${PROG}: aquiring root permission via sudo${NC}"
-sudo ls &>/dev/null
-
 if [[ $# -ne 2 ]]; then
     echo -e "${Red}${PROG} requires two args${NC}"
     echo "Usage: $0 <block device> <hostname>"
@@ -45,52 +42,45 @@ else
 fi
 
 echo -e "${Green}Creating partition table on a ${device}${NC}"
-sudo bash <<EOF
-dd if=/dev/zero of="${device}" bs=4M count=10
-parted "${device}" -- mklabel gpt
-parted "${device}" -- mkpart primary 1GB -8GB
-parted "${device}" -- mkpart primary linux-swap -8GB 100%
-parted "${device}" -- mkpart ESP fat32 1MB 1GB
-parted "${device}" -- set 3 esp on
-EOF
+sudo dd if=/dev/zero of="${device}" bs=4M count=10
+sudo parted "${device}" -- mklabel gpt
+sudo parted "${device}" -- mkpart primary 1GB -8GB
+sudo parted "${device}" -- mkpart primary linux-swap -8GB 100%
+sudo parted "${device}" -- mkpart ESP fat32 1MB 1GB
+sudo parted "${device}" -- set 3 esp on
 
 echo -e "${Green}Formatting partitions${device}${NC}"
 btrfs_part="${device}${part_prefix}1"
 boot_part="${device}${part_prefix}3"
-sudo bash <<EOF
-mkswap -L swap "${device}${part_prefix}2"
-mkfs.fat -F 32 -n BOOT "${device}${part_prefix}3"
-mkfs.btrfs -f -L nixos "${device}${part_prefix}1"
-EOF
+swap_part="${device}${part_prefix}2"
+sudo mkswap -L swap "${swap_part}"
+sudo mkfs.fat -F 32 -n BOOT "${boot_part}"
+sudo mkfs.btrfs -f -L nixos "${btrfs_part}"
 
 
 echo -e "${Green}Mounting partitions and creating BTRFS subvolumes${NC}"
 subvols=(home var nix tmp srv opt root)
-sudo bash <<EOF
-mount "${btrfs_part}" /mnt
+sudo mount "${btrfs_part}" /mnt
 btrfs subvolume create /mnt/@
 for subvol in "${subvols[@]}"; do
   echo "Creating subvol: ${subvol}..."
-  btrfs subvolume create "/mnt/@${subvol}"
+  sudo btrfs subvolume create "/mnt/@${subvol}"
 done
 echo "Disable Copy on Write on var subvol"
-chattr +C /mnt/@var
-EOF
+sudo chattr +C /mnt/@var
 
 default_subvol_id=$(btrfs subvol list /mnt | awk '/@$/{print $2; exit}')
 echo "Setting @ with vol id ${default_subvol_id} to default subvol"
 sudo btrfs subvolume set-default "${default_subvol_id}" /mnt
 
 echo "Remounting BTRFS volumes with compression"
-sudo bash <<EOF
-umount /mnt
-mount -o compress=zstd,noatime "${btrfs_part}" /mnt
-mkdir -p /mnt/{home,var,nix,tmp,srv,opt,root,boot}
+sudo umount /mnt
+sudo mount -o compress=zstd,noatime "${btrfs_part}" /mnt
+sudo mkdir -p /mnt/{home,var,nix,tmp,srv,opt,root,boot}
 for subvol in "${subvols[@]}"; do
   echo "Mounting subvol: ${subvol}..."
-  mount -o compress=zstd,noatime,subvol="@${subvol}" "${btrfs_part}" "/mnt/${subvol}"
+  sudo mount -o compress=zstd,noatime,subvol="@${subvol}" "${btrfs_part}" "/mnt/${subvol}"
 done
-EOF
 
 echo "Mounting boot partition"
 sudo mount "${boot_part}" /mnt/boot
