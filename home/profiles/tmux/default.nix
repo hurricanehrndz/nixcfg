587ffdd7557bbf4ca236e8f43aad40a2ec253221
Carlos Hernandez <carlos@hrndz.ca>
2024-10-25 17:00:34 -0600

    feat(tmux): Window switching binds compat with vim

--- home/profiles/tmux/default.nix
+++ home/profiles/tmux/default.nix
@@ -24,6 +24,18 @@ in {
       bind h 'select-pane -L'
       bind l 'select-pane -R'
 
+      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+      bind-key -n 'M-h' if-shell "$is_vim" 'send-keys M-h' 'select-pane -L'
+      bind-key -n 'M-j' if-shell "$is_vim" 'send-keys M-j' 'select-pane -D'
+      bind-key -n 'M-k' if-shell "$is_vim" 'send-keys M-k' 'select-pane -U'
+      bind-key -n 'M-l' if-shell "$is_vim" 'send-keys M-l' 'select-pane -R'
+
+      bind-key -T copy-mode-vi 'M-h' select-pane -L
+      bind-key -T copy-mode-vi 'M-j' select-pane -D
+      bind-key -T copy-mode-vi 'M-k' select-pane -U
+      bind-key -T copy-mode-vi 'M-l' select-pane -R
+
       bind-key Z switch-client -T size
 
       bind-key -T size k resize-pane -U \; switch-client -T size
