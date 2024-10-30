42cdf443bb410d5edbe2b22f1284ea6a2c294f4d
Carlos Hernandez <carlos@hrndz.ca>
2024-10-30 09:30:50 -0600

    fix(tmux): fix color and remove vim interop

--- home/profiles/tmux/default.nix
+++ home/profiles/tmux/default.nix
@@ -24,29 +24,17 @@ in {
       bind h 'select-pane -L'
       bind l 'select-pane -R'
 
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-      bind-key -n 'M-h' if-shell "$is_vim" 'send-keys M-h' 'select-pane -L'
-      bind-key -n 'M-j' if-shell "$is_vim" 'send-keys M-j' 'select-pane -D'
-      bind-key -n 'M-k' if-shell "$is_vim" 'send-keys M-k' 'select-pane -U'
-      bind-key -n 'M-l' if-shell "$is_vim" 'send-keys M-l' 'select-pane -R'
-
-      bind-key -T copy-mode-vi 'M-h' select-pane -L
-      bind-key -T copy-mode-vi 'M-j' select-pane -D
-      bind-key -T copy-mode-vi 'M-k' select-pane -U
-      bind-key -T copy-mode-vi 'M-l' select-pane -R
-
       bind-key Z switch-client -T size
 
-      bind-key -T size k resize-pane -U \; switch-client -T size
-      bind-key -T size j resize-pane -D \; switch-client -T size
-      bind-key -T size h resize-pane -L \; switch-client -T size
-      bind-key -T size l resize-pane -R \; switch-client -T size
+      bind-key -T size k resize-pane -U 5 \; switch-client -T size
+      bind-key -T size j resize-pane -D 5 \; switch-client -T size
+      bind-key -T size h resize-pane -L 5 \; switch-client -T size
+      bind-key -T size l resize-pane -R 5 \; switch-client -T size
 
-      bind-key -T size K resize-pane -U 5 \; switch-client -T size
-      bind-key -T size J resize-pane -D 5 \; switch-client -T size
-      bind-key -T size H resize-pane -L 5 \; switch-client -T size
-      bind-key -T size L resize-pane -R 5 \; switch-client -T size
+      bind-key -T size K resize-pane -U 10 \; switch-client -T size
+      bind-key -T size J resize-pane -D 10 \; switch-client -T size
+      bind-key -T size H resize-pane -L 10 \; switch-client -T size
+      bind-key -T size L resize-pane -R 10 \; switch-client -T size
 
       # begin selection with v, yank with y
       bind-key -T copy-mode-vi v send-keys -X begin-selection
@@ -77,6 +65,7 @@ in {
 
       run ${catppuccin_theme}/share/tmux-plugins/catppuccin/catppuccin.tmux
 
+      set -g "@catppuccin_session_color" "#{E:@thm_green}"
       set -g status-right "#{E:@catppuccin_status_application}#{E:@catppuccin_status_session}#{E:@catppuccin_status_date_time}"
       set -g status-left "#[bg=#{@thm_surface_1},fg=#{@thm_fg}] #{=4:client_key_table} #[fg=#{@thm_teal},bg=#{@thm_bg}]█ "
       set -gu default-command
