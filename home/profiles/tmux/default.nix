fa8b11f02769f1672ff7a3c6a3e9da28b54b6508
Carlos Hernandez <carlos@hrndz.ca>
2024-11-15 13:50:47 -0700

    Add keybindings for tmux navigator

--- home/profiles/tmux/default.nix
+++ home/profiles/tmux/default.nix
@@ -24,6 +24,16 @@ in {
       bind h 'select-pane -L'
       bind l 'select-pane -R'
 
+      is_vim="ps -o state=,tty=,comm= | grep -iqE '^[^TXZ ]+ +#{s|/dev/||:pane_tty}\s+(\\S+\\/)?g?(view|n?vim?x?|pdenv)(diff)?$'"
+      bind-key -n 'M-h' if-shell "$is_vim" 'send-keys M-h' 'select-pane -L'
+      bind-key -n 'M-j' if-shell "$is_vim" 'send-keys M-j' 'select-pane -D'
+      bind-key -n 'M-k' if-shell "$is_vim" 'send-keys M-k' 'select-pane -U'
+      bind-key -n 'M-l' if-shell "$is_vim" 'send-keys M-l' 'select-pane -R'
+      bind-key -T copy-mode-vi 'M-h' select-pane -L
+      bind-key -T copy-mode-vi 'M-j' select-pane -D
+      bind-key -T copy-mode-vi 'M-k' select-pane -U
+      bind-key -T copy-mode-vi 'M-l' select-pane -R
+
       bind-key Z switch-client -T size
 
       bind-key -T size k resize-pane -U 5 \; switch-client -T size
