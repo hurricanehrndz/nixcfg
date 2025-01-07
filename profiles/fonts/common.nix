f248cab460eac488bf43f128aa482c39832dcc64
Carlos Hernandez <carlos@hrndz.ca>
2025-01-07 08:57:24 -0700

    fix: fonts on Darwin

--- profiles/fonts/common.nix
+++ profiles/fonts/common.nix
@@ -1,27 +1,25 @@
-{ inputs
-, pkgs
-, lib
-, ...
-}:
-let
-  inherit (pkgs.stdenv.hostPlatform) isLinux isDarwin;
-  l = inputs.nixpkgs.lib // builtins;
-  fontPkgs = with pkgs; [
-    fira
-    inter
-    nerd-fonts.fira-code
-    nerd-fonts.sauce-code-pro
-    nerd-fonts.symbols-only
-  ] ++ (lib.optionals isLinux [
-    bakoma_ttf
-    corefonts # broken on aarch64-darwin
-    dejavu_fonts
-    gentium
-    liberation_ttf
-    terminus_font # broken on aarch64-darwin
-  ]);
-in
 {
-  fonts.packages = fontPkgs;
-  fonts.enableDefaultPackages = true;
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (pkgs.stdenv.hostPlatform) isLinux;
+  myFontPkgs = with pkgs;
+    [
+      fira
+      inter
+      nerd-fonts.fira-code
+      nerd-fonts.sauce-code-pro
+      nerd-fonts.symbols-only
+    ]
+    ++ (lib.optionals isLinux [
+      bakoma_ttf
+      corefonts # broken on aarch64-darwin
+      dejavu_fonts
+      gentium
+      liberation_ttf
+      terminus_font # broken on aarch64-darwin
+    ]);
+in {
+  fonts.packages = myFontPkgs;
 }
