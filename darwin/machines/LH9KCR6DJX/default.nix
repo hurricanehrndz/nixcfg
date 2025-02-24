5e015bdcd27c4bcf4860feea0caf7a7ab020f527
Carlos Hernandez <carlos@hrndz.ca>
2025-02-23 22:12:29 -0700

    Adding secrets to new system

--- darwin/machines/LH9KCR6DJX/default.nix
+++ darwin/machines/LH9KCR6DJX/default.nix
@@ -1,4 +1,5 @@
 {
+  self,
   pkgs,
   lib,
   ...
@@ -6,6 +7,21 @@
   username = "chernand";
   home = "/Users/${username}";
 in {
+ age.secrets = let
+   owner = "${username}";
+   group = "staff";
+ in {
+   "darwin/aws_auth_config" = {
+     inherit owner group;
+     file = "${self}/secrets/darwin/aws/auth_config.age";
+     path = "${home}/.aws/credentials";
+   };
+   "darwin/env/zsh_vars" = {
+     inherit owner group;
+     file = "${self}/secrets/darwin/env/zsh_vars.age";
+     path = "${home}/.config/zsh/zsh_vars";
+   };
+ };
   users.users.${username} = {
     inherit home;
     isHidden = false;
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   machineKeys = {
     lucy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF+1D/p54Xvp1lOrbl84UvY4VNtncU7SHCBdwXBCg2F";
     deepthought = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OP9bpbUbe4TWX9zRs2Yg4t3VY2Ef8GkohWvO6m/Aw";
-    VPXK04PX7G = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHX3wEseTg/3ha2mvdx/rBGj/UyjLK30pDFxmwDOjFGH";
+    LH9KCR6DJX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJDgx+dBBxdMDFHS6RBXUF+p152NrXgHkScNXmwBW2Nm";
     HX7YG952H5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJx4/mWsYx2CEjDrffAW0UqlFzkG7Kz7NIIb28KSrHxd";
   };
   userKeys = {
@@ -18,7 +18,7 @@ let
     userKeys.hurricane
   ];
   darwin_Keys= [
-    machineKeys.VPXK04PX7G
+    machineKeys.LH9KCR6DJX
     machineKeys.HX7YG952H5
     userKeys.hurricane
   ];
