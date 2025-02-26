afcba31c9af49dfc521b61985b2b19b7d950b3cf
Carlos Hernandez <carlos@hrndz.ca>
2025-02-26 08:18:14 -0700

    strongbox: init git config in repo too

--- packages/tools/strongbox-init/script.sh
+++ packages/tools/strongbox-init/script.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+if  ! grep -q strongbox .gitattributes 2>&1 /dev/null; then
 cat <<EOF >>.gitattributes
 secrets/* filter=strongbox diff=strongbox merge=strongbox
 EOF
+fi
 
-awk -F': ' '/# public key: /{print $2}' "$HOME/.strongbox_identity" > \
-    "$PWD/.strongbox_recipient"
+
+if [[ ! -f "$PWD/.strongbox_recipient" ]]; then
+    awk -F': ' '/# public key: /{print $2}' "$HOME/.strongbox_identity" > \
+        "$PWD/.strongbox_recipient"
+fi
+
+git config filter.strongbox.clean "strongbox -clean %f"
+git config filter.strongbox.smudge "strongbox -smudge %f"
+git	config filter.strongbox.required true
+git config diff.strongbox.textconv "strongbox -diff"
