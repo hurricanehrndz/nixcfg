d5623bbf33f1051019a67f09e1b7da97a98478de
Carlos Hernandez <carlos@hrndz.ca>
2025-03-03 22:29:10 -0700

    Enabling aerospace startup

--- home/profiles/tillingwm/default.nix
+++ home/profiles/tillingwm/default.nix
@@ -7,5 +7,16 @@ lib.mkMerge [
   (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
     services.tillingwm.enable = true;
     services.tillingwm.settings = builtins.readFile ./config.toml;
+    launchd.agents.aerospace = {
+      enable = true;
+      config = {
+        Program = "/Applications/AeroSpace.app/Contents/MacOS/AeroSpace";
+        ProgramArguments = [
+          "/Applications/AeroSpace.app/Contents/MacOS/AeroSpace"
+        ];
+        RunAtLoad = true;
+        KeepAlive.SuccessfulExit = false;
+      };
+    };
   })
 ]
