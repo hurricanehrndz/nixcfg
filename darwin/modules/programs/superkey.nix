3dd07034fb30c3550c0e9e2b23c93925d9ccf8fe
Carlos Hernandez <carlos@hrndz.ca>
2025-03-04 10:07:24 -0700

    Add superkey settings

--- darwin/modules/programs/superkey.nix
+++ darwin/modules/programs/superkey.nix
@@ -33,5 +33,23 @@ in {
         };
       };
     };
+    system.defaults.CustomUserPreferences = {
+      "com.knollsoft.Superkey" = {
+        "NSWindow Frame EntryBarWindow" = "1720 1151 400 40 0 0 3840 1575 ";
+        SUEnableAutomaticChecks = 0;
+        SUHasLaunchedBefore = 1;
+        capsLockRemapped = 2;
+        executeQuickHyperKey = 1;
+        hyperFlags = 1966080;
+        keyRemap = 1;
+        lastVersion = 41;
+        launchOnLogin = 0;
+        mehKeycode = 57;
+        mehRemap = 1;
+        physicalKeycode = 230;
+        quickHyperKeycode = 53;
+        statusIcon = 2;
+      };
+    };
   };
 }
