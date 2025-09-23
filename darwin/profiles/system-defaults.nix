1be8d58982c40b7951bd7e7dc01f9e2289909e57
Carlos Hernandez <carlos@hrndz.ca>
2025-09-22 20:00:33 -0600

    Add Darwin firewall settings

--- darwin/profiles/system-defaults.nix
+++ darwin/profiles/system-defaults.nix
@@ -40,6 +40,11 @@ in {
   ###: UPDATES/SECURITY/FIREWALL ================================================================
 
   system.defaults.SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+  networking.applicationFirewall.enable = true;
+  networking.applicationFirewall.enableStealthMode = true;
+  networking.applicationFirewall.allowSigned = true;
+  networking.applicationFirewall.allowSignedApp = true;
+  networking.applicationFirewall.blockAllIncoming = false;
   # Prevent nagging when opening downloaded apps.
   # system.defaults.LaunchServices.LSQuarantine = false;
 
