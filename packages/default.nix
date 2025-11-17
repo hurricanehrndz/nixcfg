cc1942266ed21d574c82bd107f090a0049c95c37
Carlos Hernandez <carlos@hrndz.ca>
2025-11-17 09:37:13 -0700

    fix: cleanup unused packages

--- packages/default.nix
+++ packages/default.nix
@@ -21,8 +21,6 @@
     in
     {
       _module.args.packages = ctx.config.packages;
-      packages.nixos-install-init = pkgs.callPackage ./tools/nixos-install-init { };
-      packages.gpt = pkgs.callPackage ./tools/gpt.nix { };
       packages.strongbox = pkgs.callPackage ./tools/strongbox.nix { inherit (inputs) strongbox-src; };
       packages.strongbox-init = pkgs.callPackage ./tools/strongbox-init { };
       packages.treefmt = pkgs.treefmt.withConfig treefmtConfig;
--- packages/tools/gpt.nix
+++
@@ -1,5 +0,0 @@
-{ writeScriptBin, pkgs, ... }:
-writeScriptBin "gpt" ''
-  args=( "$@" )
-  ${pkgs.mods}/bin/mods -f "''${args[@]}" | ${pkgs.glow}/bin/glow
-''
