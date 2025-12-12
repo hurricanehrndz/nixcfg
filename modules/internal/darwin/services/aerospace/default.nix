5c28be67a76c6c46c22b16dc7520a0d4b8e08507
Carlos Hernandez <carlos@hrndz.ca>
2025-12-11 20:28:41 -0700

    Add aerospace configuration for guiDeveloper role

---
+++ modules/internal/darwin/services/aerospace/default.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf;
+  cfg = config.hrndz;
+in
+{
+  config = mkIf cfg.roles.guiDeveloper.enable {
+    hrndz.services.aerospace.enable = true;
+    hrndz.services.aerospace.settings = builtins.readFile ./aerospace.toml;
+  };
+}
