409df24fe88a9bb5eee94b6142ae1ba80f185ac7
Carlos Hernandez <carlos@hrndz.ca>
2025-12-15 10:25:48 -0700

    Add tui helper programs

---
+++ modules/internal/darwin/programs/tools.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  cfg = config.hrndz;
+in
+{
+  config = mkIf cfg.tui.enable {
+    environment.systemPackages = with pkgs; [
+      # commandline tool for battery, volume, wifi
+      m-cli
+    ];
+  };
+}
