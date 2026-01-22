c46bd9b70bcac1c935da35c6e7932dc6a6bf6d35
Carlos Hernandez <carlos@hrndz.ca>
2026-01-22 11:05:50 -0700

    refactor(treefmt): simplify nixfmt configuration

--- per-system/treefmt.nix
+++ per-system/treefmt.nix
@@ -2,15 +2,11 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
   perSystem =
-    { pkgs, ... }:
+    { ... }:
     {
-      treefmt = with pkgs; {
+      treefmt = {
         projectRootFile = "flake.nix";
-
-        programs.nixfmt = {
-          enable = lib.meta.availableOn stdenv.buildPlatform nixfmt-rfc-style.compiler;
-          package = nixfmt-rfc-style;
-        };
+        programs.nixfmt.enable = true;
         programs.shfmt.enable = true;
         programs.shellcheck.enable = true;
         settings.formatter.shellcheck.options = [
