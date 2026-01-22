98afcc31193132591db8595fc93e44ca63ebd1f1
Carlos Hernandez <carlos@hrndz.ca>
2026-01-22 13:16:32 -0700

    feat(darwin): add nix manual

--- modules/internal/darwin/docs.nix
+++ modules/internal/darwin/docs.nix
@@ -2,17 +2,19 @@
   inputs,
   config,
   lib,
+  pkgs,
   ...
 }:
 let
   inherit (lib) mkIf;
   cfg = config.hrndz;
+  system = pkgs.stdenv.hostPlatform.system;
 in
 {
   config = mkIf cfg.roles.terminalDeveloper.enable {
     documentation.enable = true;
     environment.systemPackages = [
-      inputs.determinate-nix.packages.nix-manual
+      inputs.determinate-nix.packages.${system}.nix-manual
     ];
   };
 }
