9b94587e8d3e9a57ff427bbb7334582e448047dd
Carlos Hernandez <carlos@hrndz.ca>
2026-01-22 14:25:26 -0700

    feat(lazygit): commit via nvim

--- modules/internal/home/programs/lazygit.nix
+++ modules/internal/home/programs/lazygit.nix
@@ -13,6 +13,7 @@ in
       enable = true;
       settings = {
         promptToReturnFromSubprocess = false;
+        # moved to pdenv
         # os = {
         #   edit = "nvr -s -l {{filename}}"; # see 'Configuring File Editing' section
         #   editAtLine = "nvr -s -l +{{line}} -- {{filename}}";
@@ -32,5 +33,17 @@ in
         };
       };
     };
+    programs.zsh.initContent = lib.mkOrder 1090 ''
+      alias v="nvim"
+      if [[ -n "$NVIM" || -n "$NVIM_LISTEN_ADDRESS" ]]; then
+        export EDITOR="nvr -s -cc '"LazygitCloseFocusLargest"' &&  nvr "
+        export VISUAL="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
+        alias vi="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
+        alias vim="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
+        alias nvim="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
+        alias v="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
+      fi
+      alias gitlint="gitlint --contrib=CT1 --ignore body-is-missing,T3 -c T1.line-length=50 -c B1.line-length=72"
+    '';
   };
 }
