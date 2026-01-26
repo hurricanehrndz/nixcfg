d92495f9cf04fc02ab4ab51a28ef70a26f6e0d1b
Carlos Hernandez <carlos@hrndz.ca>
2026-01-26 08:40:00 -0700

    feat(fonts): add fonts for guiDeveloper

---
+++ modules/internal/darwin/programs/fontconfig.nix
@@ -0,0 +1,13 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkIf;
+  cfg = config.hrndz;
+in
+{
+  config = mkIf cfg.roles.guiDeveloper.enable {
+    homebrew.brews = [
+      "fontconfig"
+    ];
+  };
+}
---
+++ modules/internal/shared/fonts.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (pkgs.stdenv.hostPlatform) isLinux;
+  cfg = config.hrndz;
+in
+{
+  config = mkIf cfg.roles.guiDeveloper.enable {
+    environment.systemPackages = with pkgs; mkIf isLinux [
+        fontconfig
+    ];
+    fonts.packages =
+      with pkgs;
+      [
+        fira
+        inter
+        nerd-fonts.fira-code
+        nerd-fonts.sauce-code-pro
+        nerd-fonts.jetbrains-mono
+        nerd-fonts.hack
+        nerd-fonts.symbols-only
+      ]
+      ++ (lib.optionals isLinux [
+        bakoma_ttf
+        corefonts # broken on aarch64-darwin
+        dejavu_fonts
+        gentium
+        liberation_ttf
+        terminus_font # broken on aarch64-darwin
+      ]);
+  };
+}
