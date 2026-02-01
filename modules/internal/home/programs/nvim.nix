16956ca18cf066f59f13282d6cc94b24369b7715
Carlos Hernandez <carlos@hrndz.ca>
2026-01-31 17:11:59 -0700

    feat(nvim): do not shadow vim or nvim
    I primarily use v to edit files.

--- modules/internal/home/programs/nvim.nix
+++ modules/internal/home/programs/nvim.nix
@@ -26,8 +26,6 @@ in
         export EDITOR="nvr -s -cc '"LazygitCloseFocusLargest"' &&  nvr "
         export VISUAL="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
         alias vi="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
-        alias vim="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
-        alias nvim="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
         alias v="nvr -s -cc 'LazygitCloseFocusLargest' &&  nvr "
       fi
       alias gitlint="gitlint --contrib=CT1 --ignore body-is-missing,T3 -c T1.line-length=50 -c B1.line-length=72"
