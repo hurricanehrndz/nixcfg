ba25bbeb22b7c24e975dc0386757e9d8dab58d38
Carlos Hernandez <carlos@hrndz.ca>
2026-02-02 12:27:51 -0700

    fix: shellcheck issues

--- modules/internal/home/shells/zsh/plugins/zstyles/init.zsh
+++ modules/internal/home/shells/zsh/plugins/zstyles/init.zsh
@@ -1,3 +1,5 @@
+# vim:set ft=sh:
+# shellcheck shell=bash disable=SC2148,SC2154,SC2034,SC2086,SC2296,SC1090,SC1091
 zstyle ':zephyr:plugin:confd' directory "$ZDOTDIR/conf.d"
 zstyle ':zephyr:plugin:editor' symmetric-ctrl-z yes
 zstyle ':zephyr:plugin:editor' prepend-sudo yes
