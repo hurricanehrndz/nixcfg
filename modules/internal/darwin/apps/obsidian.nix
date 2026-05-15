c556ef3815fff486fa04d120ecc45bd741aae041
Carlos Hernandez <carlos@hrndz.ca>
2026-05-15 14:44:32 -0600

    app: add obsidian

---
+++ modules/internal/darwin/apps/obsidian.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  cfg = config.hrndz;
+in
+{
+  config = mkIf cfg.roles.guiDeveloper.enable {
+    homebrew.casks = [
+      "obsidian"
+    ];
+  };
+}
