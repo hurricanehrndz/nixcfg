f291504d008e94601a74d5c60479596b6f28dcc8
Carlos Hernandez <carlos@hrndz.ca>
2026-06-16 14:23:33 -0600

    refactor(git): aliases

--- modules/internal/home/shells/zsh/plugins/aliases/init.zsh
+++ modules/internal/home/shells/zsh/plugins/aliases/init.zsh
@@ -9,94 +9,9 @@ fi
 alias gitlint="gitlint --contrib=CT1 --ignore body-is-missing,T3 -c T1.line-length=50 -c B1.line-length=72"
 
 #######################################
-#  Git aliases
-#  Below are aliases from plugins:
-#    ga=forgit::add
-#    gat=forgit::attributes
-#    gbd=forgit::branch::delete
-#    gbl=forgit::blame
-#    gcb=forgit::checkout::branch
-#    gcf=forgit::checkout::file
-#    gclean=forgit::clean
-#    gco=forgit::checkout::commit
-#    gcp=forgit::cherry::pick::from::branch
-#    gct=forgit::checkout::tag
-#    gd=forgit::diff
-#    gfu=forgit::fixup
-#    gi=forgit::ignore
-#    gitlint='gitlint --contrib=CT1 --ignore body-is-missing,T3 -c T1.line-length=50 -c B1.line-length=72'
-#    glo=forgit::log
-#    grb=forgit::rebase
-#    grc=forgit::revert::commit
-#    grep='grep --color=auto'
-#    grh=forgit::reset::head
-#    grl=forgit::reflog
-#    grw=forgit::reword
-#    gso=forgit::show
-#    gsp=forgit::stash::push
-#    gsq=forgit::squash
-#    gss=forgit::stash::show
-#    gsw=forgit::switch::branch
-#
+# Git shortcuts live in git config and are invoked as `g <alias>`.
 #######################################
 alias g=git
-alias gaa='git add --all'
-alias gc='git commit --verbose'                \
-      gcs='git commit --verbose --sign'        \
-      gca='git commit --verbose --amend'       \
-      gcaa='git commit --verbose --amend --all'
-
-alias gdc='gd --cached'
-
-alias gcm='git checkout main'
-
-alias gst='git status' \
-      gss='git status --short'
-
-# forgit stash redefined
-alias gsv=forgit::stash::show
-
-alias grh='git reset' \
-      grhh='git reset --hard' \
-      grv=forgit::reset::head
-
-alias gf='git fetch'   \
-      gfm='git pull'    # a pull, is a fetch and merge
-
-alias gp='git push'                     \
-      gpf='git push --force'            \
-      gpF='git push --force-with-lease' \
-      gpc='git push --set-upstream origin HEAD'
-
-# git clone
-alias gcl='git clone'
-
-# git branch
-alias gb='git branch' \
-      gba='git branch -a'
-
-# git merge
-alias gm='git merge'
-
-# git show
-alias gsh='git show'
-
-# git stash
-alias gsta='git stash'
-
-# Git rebase sign commits
-alias grsc='git rebase --exec "git commit --amend --no-edit -n -S" -i'
-
-# git log
-_git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d %C(reset)%C(blue)Sig:%G?%C(reset)%n'
-_git_log_brief_format='%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
-
-_git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n'
-_git_log_medium_format+='%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n'
-_git_log_medium_format+='%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
-alias gl='git log --topo-order --pretty=format:"$_git_log_medium_format"' \
-      glb='git log --topo-order --pretty=format:"$_git_log_brief_format"' \
-      glg='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_format}"'
 
 #######################################
 # ls alternatives
@@ -142,5 +57,6 @@ alias rgv="rg --line-number --with-filename --color=always --field-match-separat
   fzf --ansi --delimiter ' ' \
     --preview 'bat --color=always --highlight-line {2} {1}' \
     --preview-window 'right,50%,+{2}-3,~3'"
+alias mdcat='glow'
 alias clauded='claude --dangerously-skip-permissions'
 alias pi='env -u AWS_PROFILE pi'
