1a894b6a4196e1dab8b777345520ed849b2dea1b
Carlos Hernandez <chernand@yelp.com>
2026-06-20 18:41:05 -0600

    ghostty: disable tabs and add padding

--- modules/internal/home/apps/ghostty.nix
+++ modules/internal/home/apps/ghostty.nix
@@ -55,9 +55,11 @@ in
         # window-decoration = false;
         shell-integration-features = "sudo,no-ssh-terminfo,no-ssh-env,cursor";
         clipboard-paste-protection = false;
-        window-show-tab-bar = "never";
-        # Prefer windows over tabs: make ⌘T open a new window like ⌘N.
-        keybind = "super+t=new_window";
+        # window-show-tab-bar = "never";
+        # # Prefer windows over tabs: make ⌘T open a new window like ⌘N.
+        # keybind = "super+t=new_window";
+        macos-titlebar-style = "hidden";
+        window-padding-y = "6,0";
       };
     };
   };
